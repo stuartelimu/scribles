6e2fa096ee9f6b45b78ac77f1a5a5f65e5aee1f3
Stuart Elimu <41069456+stuartelimu@users.noreply.github.com>
2021-07-10 20:00:54 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Nikola Tesla"  # (translatable)
 BLOG_TITLE = "Hello, world!"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://stuartelimu.github.io/scribles/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
